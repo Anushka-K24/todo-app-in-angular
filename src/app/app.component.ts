20ae8fc70d4a89b152f0cb08746706723e708e39
Anushka-K24 <anushka.kalamkar24@gmail.com>
2021-05-05 15:12:47 +0530

    made a new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-new-app';
+  constructor() {
+setTimeout(() => {
+  this.title = 'Changed Title';
+}, 5000);
+  }
+  
 }
